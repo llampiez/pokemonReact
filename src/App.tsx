96775911df706d236dab30b085da266a5fde517a
luistapls <llopezampiez7@gmail.com>
2023-10-07 01:39:58 -0400

    new logo pokemon

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import { RandomPokemons } from './components/RandomPokemons'
+import test from './public/pokemonLogo.png'
 
 export const App = (): JSX.Element => {
   return (
     <>
+      <div>
+        <img src={test}/>
+      </div>
       <RandomPokemons/>
     </>
   )
--- src/components/RandomPokemons.tsx
+++ src/components/RandomPokemons.tsx
@@ -4,15 +4,15 @@ export const RandomPokemons = (): JSX.Element => {
   const objPokemonArray = usePokemon()
 
   return (
-      <div>
+      <div className='grid grid-rows-1 grid-cols-5 justify-items-center'>
         {objPokemonArray?.map((objPokemon: any): JSX.Element => {
           const { id, name, sprites } = objPokemon
           const { other: { 'official-artwork': { front_default: imgUrlPokemon } } } = sprites
 
           return (
-            <div key={id}>
-              <img src={imgUrlPokemon}/>
-              <h1 className='text-3xl font-bold underline text-red-600'>{name}</h1>
+            <div className='grid grid-rows-2 grid-cols-1 justify-items-center' key={id}>
+              <img className='w-10 h-10' src={imgUrlPokemon}/>
+              <h1>{name}</h1>
             </div>
           )
         })}
